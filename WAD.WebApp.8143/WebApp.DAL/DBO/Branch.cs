66d7afe80d784cc08c4970d67fa68b7886cf738b
Mullakhuja <cccxoja@gmail.com>
2021-06-23 02:17:25 +0500

    model class created, Branch and Employee

---
+++ WAD.WebApp.8143/WebApp.DAL/DBO/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WebApp.DAL.DBO
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Branch Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ WAD.WebApp.8143/WebApp.DAL/DBO/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebApp.DAL.DBO
+{
+    public class Employee
+    {
+        [DisplayName("Id")]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public byte[] EmpPhotoBinary { get; set; }
+
+        public bool IsFullTime { get; set; }
+
+        [Required]
+        [Display(Name = "Branch")]
+        public int BranchId { get; set; }
+
+        public Branch Branch { get; set; }
+    }
+}
